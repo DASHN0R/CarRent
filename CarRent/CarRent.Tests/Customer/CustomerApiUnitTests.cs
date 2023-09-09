b989851196800cfaf8fe9da95aa4c9420c378195
Dashnor <Dashnor@DESKTOP-I4RQRTM>
2023-09-09 20:56:03 +0200

    add tests

---
+++ CarRent/CarRent.Tests/Customer/CustomerApiUnitTests.cs
@@ -0,0 +1,50 @@
+﻿using CarRent.Customer.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CarRent.Tests.Customer
+{
+    public class CustomerApiUnitTests
+    {
+        [Fact]
+        public void CustomerDtoConstructor_CreateCustomerDtoBasedOnCustomer()
+        {
+            //Arrange
+            var id = new Guid();
+            var street = "Gaiserwaldstrasse 6";
+            var city = "9015 St. Gallen";
+            var firstName = "Dashnor";
+            var lastName = "Shala";
+
+            var customer = new CarRent.Customer.Domain.Customer(id)
+            {
+                Street = street,
+                City = city,
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            var expectedCustomerDto = new CustomerResponseDto()
+            {
+                Id = id,
+                Street = street,
+                City = city,
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            //Act
+            var createdCustomerDto = new CustomerResponseDto(customer);
+
+            //Assert
+            Assert.Equal(expectedCustomerDto.Street, createdCustomerDto.Street);
+            Assert.Equal(expectedCustomerDto.FirstName, createdCustomerDto.FirstName);
+            Assert.Equal(expectedCustomerDto.LastName, createdCustomerDto.LastName);
+            Assert.Equal(expectedCustomerDto.City, createdCustomerDto.City);
+        }
+    }
+}
---
+++ CarRent/CarRent.Tests/Customer/CustomerDomainUnitTests.cs
@@ -0,0 +1,52 @@
+﻿using CarRent.Car.Domain;
+using CarRent.Customer.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CarRent.Tests.Customer
+{
+    public class CustomerDomainUnitTests
+    {
+        [Fact]
+        public void CustomerConstructor_CreateCustomerBasedOnDto()
+        {
+            //Arrange
+            var id = new Guid();
+            var street = "Gaiserwaldstrasse 6";
+            var city = "9015 St. Gallen";
+            var firstName = "Dashnor";
+            var lastName = "Shala";
+
+            var expectedCustomer = new CarRent.Customer.Domain.Customer(id)
+            {
+                Street = street,
+                City = city,
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            var customerDto = new CustomerResponseDto()
+            {
+                Id = id,
+                Street = street,
+                City = city,
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            //Act
+            var createdCustomer = new CarRent.Customer.Domain.Customer(customerDto);
+
+            //Assert
+            Assert.Equal(expectedCustomer, createdCustomer);
+            Assert.Equal(expectedCustomer.Street, createdCustomer.Street);
+            Assert.Equal(expectedCustomer.FirstName, createdCustomer.FirstName);
+            Assert.Equal(expectedCustomer.LastName, createdCustomer.LastName);
+            Assert.Equal(expectedCustomer.City, createdCustomer.City);
+        }
+    }
+}
